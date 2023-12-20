e83e71b4a937229d9727a98614cb0cc80ca87884
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-12-20 11:24:52 -0500

    [+] index.js: Implementacion de CORS

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
+const cors = require('cors');
 
+app.use(cors()); // Usa cors middleware
 app.use(express.json()); // Middleware para parsear el cuerpo de las solicitudes como JSON
 
 /** Configuración de Base de datos */
